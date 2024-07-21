30aaf0219c084fa5aaf0f1de2dfc2b72701604fc
Danish Khan <112203113+letscodedanish@users.noreply.github.com>
2024-07-21 18:15:23 +0530

    Create Index of the First Occurrence in a String.cpp

---
+++ Index of the First Occurrence in a String.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int haylength=haystack.length();
+        int needlelength=needle.length();
+        if(haylength<needlelength)
+            return -1;
+        for(int j=0;j<=haystack.size()-needle.size();j++){
+            int i=0;
+            while(i<needle.size() && haystack[i+j] == needle[i]){
+                i++;
+            }
+            if(i == needle.size()){
+                return j;
+            }
+        }
+        return -1;
+    }
+};
+
+/*
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+ 
+
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+Example 2:
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+ 
+
+Constraints:
+
+1 <= haystack.length, needle.length <= 104
+haystack and needle consist of only lowercase English characters.
+*/
